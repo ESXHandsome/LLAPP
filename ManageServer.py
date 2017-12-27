ced9a9a1ea921a818e770c2072a734bc42c988db
shizitou <shizitou@lingyongqian.cn>
2017-12-27 15:02:46 +0800

    完成登录模块服务端

--- ManageServer.py
+++ ManageServer.py
@@ -3,26 +3,41 @@
 from flask import Flask
 import json
 from flask import request
-
+import pymongo
 app = Flask(__name__)
 
+client = pymongo.MongoClient()
+db = client.ll_server
+collection = db.login
+
+#login
 @app.route('/login',methods=['POST'])
 def login():
-    print "请求网络"
     user = request.form.get('name')
     pwd = request.form.get('pwd')
+    dic = collection.find_one({"user":user})
+    print dic
+    if dic != None:
+        print  dic[u'password']
+        if dic[u'password'] == pwd:
+            return json.dumps({'status':'login'})
     if user == 'admin' and pwd == "admin":
         return json.dumps({'status':'login'})
     else:
         return json.dumps({'status':'unlogin'})
 
+
 @app.route('/')
 def hello_name():
     return 'this is name\n'
 
+
 @app.route('/show')
 def show():
     return 'this is show \n'
+
+
+
 
 if __name__ == '__main__':
     app.debug = True
